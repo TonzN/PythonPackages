7f3f690fab6770046e2b7fb23d333875c8abc734
TonzN <66659994+TonzN@users.noreply.github.com>
2023-02-14 12:43:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='DevelopmentPackages',
     version='0.0.2.5',
-    packages=["Python"],
+    packages=["PythonPackages"],
     install_requires=[
           'numpy',
           "pygame",
